df62b0aadf7746252e5868f28095aa854726f26d
T0kudai <vishalvsy1996@gmail.com>
2021-12-08 12:45:47 +0530

    Formatting

--- Assignment 2/CollusionDetect_4_1.py
+++ Assignment 2/CollusionDetect_4_1.py
@@ -1,10 +1,10 @@
 ###############################################################################################
 
-#Implementation of Shared Nearest Neighbour Graph Clustering Algorithm to Detect Collusion sets
-#Contributors:
-#Vishal Singh Yadav: CS20MTECH01001
-#Anilava Kundu: CS20MTECH01002
-#Kuldeep Gautam: CS20MTECH01004
+# Implementation of Shared Nearest Neighbour Graph Clustering Algorithm to Detect Collusion sets
+# Contributors:
+# Vishal Singh Yadav: CS20MTECH01001
+# Anilava Kundu: CS20MTECH01002
+# Kuldeep Gautam: CS20MTECH01004
 
 ###############################################################################################
 
@@ -12,41 +12,40 @@ import pandas as pd
 import networkx as nx
 
 
-#Creates a Map from a vertex to a cluster it is present in
-def verticeToCluster(G,clusters):
+# Creates a Map from a vertex to a cluster it is present in
+def verticeToCluster(G, clusters):
 
-    vert2Clust={}
+    vert2Clust = {}
 
     for u in G.nodes:
-        for i in range(0,len(clusters)):
+        for i in range(0, len(clusters)):
             if u in clusters[i]:
-                vert2Clust[u]=clusters[i]
+                vert2Clust[u] = clusters[i]
 
     return vert2Clust
 
 
-#Implementation of Shared Nearest Neighbours Algorithm
-def findCollusionSets(G,dict_kNN,kt):
- 
-    vertices=G.nodes
-    
-    clusters=[[x] for x in vertices]
+# Implementation of Shared Nearest Neighbours Algorithm
+def findCollusionSets(G, dict_kNN, kt):
 
-    vert2Clust=verticeToCluster(G,clusters)
+    vertices = G.nodes
 
-    KT=kt
+    clusters = [[x] for x in vertices]
 
-    #Algorithm given in the paper
+    vert2Clust = verticeToCluster(G, clusters)
+
+    KT = kt
+
+    # Algorithm given in the paper
     for u in vertices:
         for v in vertices:
-            l=len([value for value in dict_kNN[u] if value in dict_kNN[v]]) 
-            c_u=vert2Clust[u]
-            if v not in c_u and l >= KT and u in dict_kNN[v] and v in dict_kNN[u]: 
-                
-                c_v=vert2Clust[v]
+            l = len([value for value in dict_kNN[u] if value in dict_kNN[v]])
+            c_u = vert2Clust[u]
+            if v not in c_u and l >= KT and u in dict_kNN[v] and v in dict_kNN[u]:
 
-                c_u_v=c_u+c_v
+                c_v = vert2Clust[v]
 
+                c_u_v = c_u + c_v
 
                 clusters.remove(c_v)
                 clusters.remove(c_u)
@@ -54,48 +53,53 @@ def findCollusionSets(G,dict_kNN,kt):
                 clusters.append(c_u_v)
 
                 for x in c_u_v:
-                    vert2Clust[x]=c_u_v
+                    vert2Clust[x] = c_u_v
 
-
-    
-    collude_set=[]
+    collude_set = []
     for x in clusters:
-        if(len(x)>=2):
+        if len(x) >= 2:
             collude_set.append(x)
 
     return collude_set
 
 
-#Creates a dictionary for kNN for each vertex
-def find_KNN(G,k):
-    
-    dict_kNN={}
+# Creates a dictionary for kNN for each vertex
+def find_KNN(G, k):
+
+    dict_kNN = {}
 
     for u in G.nodes:
-        #Sorting neighbours in descending order according to Amount[more amount traded ===> less distance] 
-        sort_dict=sorted(G[u].items(),key=lambda edge: edge[1]['Amount'],reverse=True)
-        sorted_neighbours=[]
+        # Sorting neighbours in descending order according to Amount[more amount traded ===> less distance]
+        sort_dict = sorted(
+            G[u].items(), key=lambda edge: edge[1]["Amount"], reverse=True
+        )
+        sorted_neighbours = []
         for v in sort_dict:
             sorted_neighbours.append(v[0])
-            #taking top k neighbours
-            dict_kNN[u]=sorted_neighbours[0:k]
-        if(len(sort_dict)==0):
-            dict_kNN[u]=[]
+            # taking top k neighbours
+            dict_kNN[u] = sorted_neighbours[0:k]
+        if len(sort_dict) == 0:
+            dict_kNN[u] = []
     return dict_kNN
 
 
+if __name__ == "__main__":
 
-if __name__=="__main__":
+    df = pd.read_csv("cluster.csv")
 
-    df=pd.read_csv('cluster.csv')
-    
-    Graphtype=nx.DiGraph()
+    Graphtype = nx.DiGraph()
 
-    kt=2
-    K=4
+    kt = 2
+    K = 4
 
-    G=nx.from_pandas_edgelist(df,source='Vertex 1',target='Vertex 2',edge_attr='Amount',create_using=Graphtype)
-    
-    dict_kNN=find_KNN(G,K)
-    
-    print(findCollusionSets(G,dict_kNN,kt))
+    G = nx.from_pandas_edgelist(
+        df,
+        source="Vertex 1",
+        target="Vertex 2",
+        edge_attr="Amount",
+        create_using=Graphtype,
+    )
+    dict_kNN = find_KNN(G, K)
+
+    print(findCollusionSets(G, dict_kNN, kt))
+
--- Assignment 2/CollusionDetect_4_2.py
+++ Assignment 2/CollusionDetect_4_2.py
@@ -1,95 +1,101 @@
-
 import pandas as pd
 import networkx as nx
 import sys
 
-def find_KNN(G,k):
 
-    dict_kNN={}
+def find_KNN(G, k):
+
+    dict_kNN = {}
     for u in G.nodes:
-        sort_dict=sorted(G[u].items(),key=lambda edge: edge[1]['Amount'],reverse=True)
-        count=0
-        sorted_neighbours=[]
+        sort_dict = sorted(
+            G[u].items(), key=lambda edge: edge[1]["Amount"], reverse=True
+        )
+        count = 0
+        sorted_neighbours = []
         for v in sort_dict:
             if count >= k:
-                G.remove_edge(u,v[0])
+                G.remove_edge(u, v[0])
                 continue
-            count+=1
+            count += 1
             sorted_neighbours.append(v[0])
 
-        dict_kNN[u]=sorted_neighbours
-    
+        dict_kNN[u] = sorted_neighbours
+
     return dict_kNN
 
 
+def calculateMNV(C1, C2, dict_kNN):
 
-def calculateMNV(C1,C2,dict_kNN):
-
-    mnv_list=[]
+    mnv_list = []
     for v1 in C1:
         for v2 in C2:
             if v2 in dict_kNN[v1] and v1 in dict_kNN[v2]:
-                #print(v2,dict_kNN[v1])
-                #print(v1,dict_kNN[v2])
-                sum_mnv=dict_kNN[v1].index(v2)+1 + dict_kNN[v2].index(v1)+1
-                #print(sum_mnv)
+                # print(v2,dict_kNN[v1])
+                # print(v1,dict_kNN[v2])
+                sum_mnv = dict_kNN[v1].index(v2) + 1 + dict_kNN[v2].index(v1) + 1
+                # print(sum_mnv)
                 mnv_list.append(sum_mnv)
             elif v2 in dict_kNN[v1] and v1 not in dict_kNN[v2]:
-                sum_mnv=dict_kNN[v1].index(v2)+100
+                sum_mnv = dict_kNN[v1].index(v2) + 100
                 mnv_list.append(sum_mnv)
             elif v2 not in dict_kNN[v1] and v1 in dict_kNN[v2]:
-                sum_mnv=dict_kNN[v2].index(v1)+100
+                sum_mnv = dict_kNN[v2].index(v1) + 100
                 mnv_list.append(sum_mnv)
             else:
                 mnv_list.append(200)
 
-    avg_mnv=sum(mnv_list)/len(mnv_list)
- 
+    avg_mnv = sum(mnv_list) / len(mnv_list)
+
     return avg_mnv
 
 
-def find_clusters(G,m,dict_kNN):
-    
-    vertices=G.nodes
-    
-    clusters=[[x] for x in vertices]
+def find_clusters(G, m, dict_kNN):
 
-    min_mnv=sys.maxsize
-    min_c1=None
-    min_c2=None
+    vertices = G.nodes
 
-    #while len(clusters) > m:
+    clusters = [[x] for x in vertices]
+
+    min_mnv = sys.maxsize
+    min_c1 = None
+    min_c2 = None
+
+    # while len(clusters) > m:
     for x in clusters:
         for y in clusters:
-            if x==y:
+            if x == y:
                 continue
-            mnv=calculateMNV(x,y,dict_kNN)
+            mnv = calculateMNV(x, y, dict_kNN)
             if mnv <= min_mnv:
-                min_mnv=mnv
-                min_c1=x
-                min_c2=y
+                min_mnv = mnv
+                min_c1 = x
+                min_c2 = y
 
-        
     print(min_mnv)
-        #min_c1_c2=min_c1+min_c2
-        #clusters.remove(min_c1)
-        #clusters.remove(min_c2)
-        #clusters.append(min_c1_c2)
+    # min_c1_c2=min_c1+min_c2
+    # clusters.remove(min_c1)
+    # clusters.remove(min_c2)
+    # clusters.append(min_c1_c2)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
 
-    df=pd.read_csv('cluster.csv')
-    
-    Graphtype=nx.DiGraph()
+    df = pd.read_csv("cluster.csv")
 
-    kt=2
-    K=4
+    Graphtype = nx.DiGraph()
 
-    G=nx.from_pandas_edgelist(df,source='Vertex 1',target='Vertex 2',edge_attr='Amount',create_using=Graphtype)
-    
-    #print(G.nodes)
-    
-    dict_kNN=find_KNN(G,K)
+    kt = 2
+    K = 4
 
-    find_clusters(G,5,dict_kNN)
+    G = nx.from_pandas_edgelist(
+        df,
+        source="Vertex 1",
+        target="Vertex 2",
+        edge_attr="Amount",
+        create_using=Graphtype,
+    )
+    # print(G.nodes)
+
+    dict_kNN = find_KNN(G, K)
+
+    find_clusters(G, 5, dict_kNN)
+
